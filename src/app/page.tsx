7d4e3bda0602aad1963badf7244fc311d62bcd6e
Danny Suggi Saputra <dannysaputra3003@gmail.com>
2023-07-08 21:30:33 +0700

    feat: text transition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,21 +9,35 @@ export default function Home() {
           <div>
             <h3 className='pt-2'>Welcome to <span className='text-red-400'>R.S.V.P. JAKARTA</span></h3>
           </div>
-          <div>
+          {/* <div>
             <h3 className='pt-2'>Your Boutique PR & Communications Agency</h3>
+          </div> */}
+          <h3>Your</h3>
+          <div id='flip'>
+            <div>
+              <h3>Boutique PR</h3>
+            </div>
+            <div>
+              <h3>Communications Agency</h3>
+            </div>
           </div>
         </h1>
       </div>
-      <section className='flex w-3/5 mt-10 ml-6'>
-        <p>
-        At R.S.V.P. JAKARTA, we are a dynamic and integrated communications agency specializing in public relations, events, content creation, and brand strategy. With a diverse clientele spanning luxury, lifestyle, hospitality, F&B, education, embassy, art, and culture, we bring extensive expertise and a passion for excellence to every project we undertake.
-        </p>
-      </section>
-      <section className='flex w-3/5 mt-10'>
-        <div className="justify-end">
-          <p>
-          Established in 2020, we have quickly become a trusted advisor in communications, PR, and strategy for numerous brands and businesses entering the Indonesian market. Today, we stand as a boutique agency with a reputation for delivering results-driven solutions that set our clients apart.
+      <section className="w-1/2 mt-10 ml-2">
+        <div className="px-3 py-4">
+          <p className="text-base text-justify">
+          At R.S.V.P. JAKARTA, we are a dynamic and integrated communications agency specializing in public relations, events, content creation, and brand strategy. With a diverse clientele spanning luxury, lifestyle, hospitality, F&B, education, embassy, art, and culture, we bring extensive expertise and a passion for excellence to every project we undertake.
           </p>
+        </div>
+      </section>
+      <section className="flex mr-2">
+        <div className="w-1/2"></div>
+        <div className="w-1/2 mt-5">
+          <div className="px-3 py-4">
+            <p className="text-base text-justify">
+            Established in 2020, we have quickly become a trusted advisor in communications, PR, and strategy for numerous brands and businesses entering the Indonesian market. Today, we stand as a boutique agency with a reputation for delivering results-driven solutions that set our clients apart.
+            </p>
+          </div>
         </div>
       </section>
     </main>
