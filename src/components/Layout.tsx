82d6aa0696071a994b521fa76eaad1fd4304b4cb
azharjk <azhr.project@gmail.com>
2023-07-10 20:30:50 +0700

    feat: add location

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -13,8 +13,8 @@ export default function Layout({ children } : { children: React.ReactNode }) {
 					<span className="text-[42px]">RSVP Jakarta</span>
 				</div>
 			<div className={`col-[8] flex justify-end items-center ${isOpen ? 'mr-2 transition-[margin] delay-500' : '-mr-32'}`}>
-          <button onClick={() => setIsOpen(false)}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-24 h-24">
+          <button onClick={() => setIsOpen(false)} className="group">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-24 h-24 transition group-hover:text-red-400">
   						<path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
 						</svg>
           </button>
@@ -22,18 +22,23 @@ export default function Layout({ children } : { children: React.ReactNode }) {
 				<div className="w-full h-full row-[3_/_span_3] md:row-[2_/_span_3] col-span-8">
 					<ul className="w-full h-full flex flex-col items-strecth justify-center text-center">
 						<li className="grid md:grid-cols-8 py-2 border-t border-gray-700 transition hover:bg-red-400">
-							<span className="md:col-[3] text-[72px]">Art</span>
+							<span className="md:col-[3] text-[72px] uppercase">Art</span>
 							<span className="col-[5_/_span_2] hidden md:flex items-center text-justify">Lorem ipsum dolor sit amet</span>
 						</li>
 						<li className="grid md:grid-cols-8 py-2 border-t border-gray-700 transition hover:bg-red-400">
 							<span className="col-[2_/_span_3] hidden md:flex items-center text-justify">Praesent laoreet venenatis purus</span>
-							<span className="md:col-[6] text-[72px]">Work</span>
+							<span className="md:col-[6] text-[72px] uppercase">Work</span>
 						</li>
 						<li className="grid md:grid-cols-8 py-2 border-y border-gray-700 transition hover:bg-red-400">
-							<span className="md:col-[2] lg:col-[3] text-[72px]">Contact</span>
+							<span className="md:col-[2] lg:col-[3] text-[72px] uppercase">Contact</span>
 							<span className="col-[6_/_span_2] hidden md:flex items-center text-justify">Aliquam vel fringilla risus.</span>
 						</li>
 					</ul>
+				</div>
+				<div className="hidden md:block col-[4_/_span_2]">
+					<p className="text-center">1600 Amphitheatre Parkway</p>
+					<p className="text-center">Mountain View, CA 94043, USA</p>
+					<p className="text-center">(650) 253-0000</p>
 				</div>
 			</nav>
 
